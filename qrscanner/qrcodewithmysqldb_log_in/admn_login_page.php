3934412383e3147b4a128f8b338cd0b26a659225
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-11-26 22:34:55 +0800

    uploading of qrscanner website

---
+++ qrscanner/qrcodewithmysqldb_log_in/admn_login_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>ADMIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+     	<input type="text" name="uname" placeholder="User Name"><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+
+     	<button type="submit">Login</button>
+		<a href="index.php">home</a>
+     </form>
+</body>
+</html>
---
+++ qrscanner/qrcodewithmysqldb_logging_out/admn_page.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Page</title>
+    <style>
+        table{
+            border-collapse: collapse;
+            width: 50%;
+        }
+        table th{
+            background-color: gray;
+            color: #fff;
+            padding: 10px;
+        }
+        table td{
+            padding: 12px;
+            color: black;
+            font-size: 1em;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+<?php
+
+    include "db_conn.php";
+
+    $sql = "SELECT * FROM logs_out;";
+    $sql1 = "SELECT * FROM logs_in;";
+
+    $results = mysqli_query($conn, $sql);
+    $results1 = mysqli_query($conn, $sql1);
+    $resultCheck = mysqli_num_rows($results);
+    $resultCheck1 = mysqli_num_rows($results1);
+?>
+
+    <table>
+        <tr>
+            <th>Name and Section</th>
+            <th>Time In</th>
+        </tr>
+        <tr>
+        <?php
+            if ($resultCheck1 > 0){
+                while($row1 = mysqli_fetch_assoc($results1)){
+                    echo "
+                        <tr>
+                        <td>".$row1['name_and_section']."</td>
+                        <td>".$row1['Time_in']."</td>
+                        </tr>
+                    ";
+                }
+            }
+        ?>
+    </table>
+    <table>
+        <tr>
+            <th>Name and Section</th>
+            <th>Time Out</th>
+        </tr>
+        <tr>
+        <?php
+            if ($resultCheck > 0){
+                while($row = mysqli_fetch_assoc($results)){
+                    echo "
+                        <tr>
+                        <td>".$row['name_and_section']."</td>
+                        <td>".$row['Time_out']."</td>
+                        </tr>
+                    ";
+                }
+            }
+        ?>
+        </tr>
+    </table>
+
+    <a href="logout.php">Logout</a>
+
+</body>
+
+</html>
+<?php
+
+}else{
+    header("Location: admn_login_page.php");
+    exit();
+}
